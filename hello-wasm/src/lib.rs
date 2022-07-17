c1190bb0162e616e5b810645bf3c029d5c4c3df8
from20020516 <lofi.personal@gmail.com>
2022-07-17 09:53:04 +0000

    Add `console.log`

--- hello-wasm/src/lib.rs
+++ hello-wasm/src/lib.rs
@@ -4,10 +4,16 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
+    // https://developer.mozilla.org/ja/docs/WebAssembly/Rust_to_wasm#lets_write_some_rust
     pub fn alert(s: &str);
+
+    // https://rustwasm.github.io/wasm-bindgen/examples/console-log.html
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
+    log(&format!("Hey, {}!", name));
 }
